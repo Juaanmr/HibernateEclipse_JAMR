6fab3009a2e9fa7b6af4d86c35e6cfe5ad741ef4
Juaanmr <juanagus0718@gmail.com>
2024-01-22 19:32:12 +0100

    Primera versión
    Despues de unas semanas logramos implementar Hibernate con Eclipse
    Adjunto una copia de la base de datos la cual he utilizado para esta practica 3

---
+++ ejemploHibernate/src/main/java/dam2/ejemploHibernate/UseHibernateEnterprise.java
@@ -0,0 +1,9 @@
+package dam2.ejemploHibernate;
+
+public class UseHibernateEnterprise {
+
+	public static void main(String[] args) {
+		HibernateEnterprise h=new HibernateEnterprise();
+		h.addProduct(9, "teclado",170);
+	}
+}
